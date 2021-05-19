387b0e10df3298d2965a5dfbb18f42f53d141f74
somnathmistry-app <somnath222666@gmail.com>
2021-05-19 21:25:52 +0530

    initial commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_topics/search/search.dart';
+import 'package:flutter_topics/sqflite/login.dart';
 import 'package:get/get.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -11,15 +13,20 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(title: Text('Topics'),),
       body: Column(
         children: [
-          Center(
-            child: ElevatedButton(
+             ElevatedButton(
               child: Text('SearchView'),
                 onPressed: (){
               Get.to(
                   SearchView()
               );
             }),
-          )
+          ElevatedButton(
+              child: Text('SearchView'),
+              onPressed: (){
+                Get.to(
+                    LoginSql()
+                );
+              }),
         ],
       ),
     );
